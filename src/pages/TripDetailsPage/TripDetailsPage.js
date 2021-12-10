e6405bac557ee61c2b5125a585091c7e55f6e54c
Karina Freitas <karinaglf@gmail.com>
2021-12-10 12:01:49 +0000

    add TripDetailsPage base

--- src/pages/TripDetailsPage/TripDetailsPage.js
+++ src/pages/TripDetailsPage/TripDetailsPage.js
@@ -1,10 +1,41 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
 
-
+const API_URL = process.env.REACT_APP_SERVER_URL || 'http://localhost:5005';
 
 function TripDetailsPage() {
+    const [trip, setTrip] = useState(null)
+
+    const { tripId } = useParams();
+  // Make an axios call when the component is created
+  // and get the trip details from the server
+
+  const getTrip = async () => {
+    try {
+      const response = await axios.get(`http://localhost:5005/api/trips/${tripId}`);
+      const oneTrip = response.data;
+      setTrip(oneTrip);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getTrip();
+  }, [])
+
     return ( 
-        <div>
-            <h2>Trip Details Page</h2>
+        <div className="">
+        {trip && (
+          <>
+            <h1>{trip.tripName}</h1>
+  
+            <Link to={`/edit-project/${trip._id}`}>
+              <button>Edit Project</button>
+            </Link>
+          </>  
+        )}
         </div>
      );
 }
