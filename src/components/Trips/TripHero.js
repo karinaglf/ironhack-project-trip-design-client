cded2dc35ef49429fc4f8cef8787eb4c096475e2
Karina Freitas <karinaglf@gmail.com>
2021-12-15 15:25:54 +0000

    finish styling trip meta bar

--- src/components/Trips/TripHero.js
+++ src/components/Trips/TripHero.js
@@ -4,56 +4,60 @@ import EventAvailableIcon from '@mui/icons-material/EventAvailable';
 import MapIcon from '@mui/icons-material/Map';
 import PersonAddIcon from '@mui/icons-material/PersonAdd';
 import { flexbox } from '@mui/system';
+import { chainPropTypes } from '@mui/utils';
 
 
 
-function TripHero({tripName, coverImg, pax }) {
-    
+function TripHero({tripName, coverImg, pax, destination, duration, ...props }) {
+
+ let cities = destination.map(element => {
+    return element.city.name
+  })
+
     const styleContainer = {
-        height: 500,
+        height: 580,
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
         flexDirection: "column",
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
+        backgroundPosition: "center center",
         boxShadow: "inset 0 0 0 2000px rgba(32, 32,34, 0.3)"
     }
-
-    const styleListWrap = {
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center"
-    }
-
     return ( 
         <Box fullWidth sx={{...styleContainer, backgroundImage: `url(${coverImg})`
         }}
       >
-        <Typography variant="h2" sx={{ fontWeight: 'bold'}}>{tripName}</Typography>
-        <Box sx={{ width: '100%', maxWidth: 800, bgcolor: 'background.paper', display: 'flex' }}>
-        <List style={{ display: 'flex', flexDirection: 'row', padding: 0 }}>
-          <ListItem disablePadding>
+        <h1 style={{fontSize: "4rem", color:"white"}}>{tripName}</h1>
+        <Box sx={{ width: '100%', maxWidth: 680, bgcolor: 'background.paper', display: 'flex', padding: '20px 0', borderRadius: '10px' }}>
+        <Grid container alignItems="center" justifyContent="center">
+        <Grid item xs={12} md={3} align="center">
+        <ListItem alignItems="center" disablePadding>
               <ListItemIcon>
                 <EventAvailableIcon />
               </ListItemIcon>
-              <ListItemText primary={tripName} />
+              <ListItemText primary={`${duration} days`} />
           </ListItem>
-          <ListItem disablePadding>
+        </Grid>
+        <Grid item xs={12} md={6}>
+        <ListItem  alignItems="center">
               <ListItemIcon>
                 <MapIcon />
               </ListItemIcon>
-              <ListItemText primary="Drafts" />
+              <ListItemText primary={cities.join(', ')} />
           </ListItem>
-          <ListItem disablePadding>
+        </Grid>
+        <Grid item xs={12} md={3}>
+        <ListItem alignItems="center">
               <ListItemIcon>
                 <PersonAddIcon />
               </ListItemIcon>
               <ListItemText primary={`${pax} travelers`} />
           </ListItem>
-        </List>
-    </Box>
-        
+        </Grid>
+        </Grid>
+      </Box>
       </Box>
      );
 }
